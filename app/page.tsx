f49a1b608cb3e2c43b9ce818f495608bd98dff6d
gon <gonefdez@gmail.com>
2025-06-17 20:41:27 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <RoomsSection />
       <ProcessSection />
       <TestimonialsSection />
-      <CTASection />
+      {/* <CTASection /> */}
     </>
   );
 }
